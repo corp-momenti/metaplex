f1b822f2ad53661d6ce695b63056be48882e8734
Hany Rock <rock@momenti.tv>
2022-01-20 15:33:24 +0900

    change devnet endpoint

--- js/packages/token-entangler/src/contexts/ConnectionContext.tsx
+++ js/packages/token-entangler/src/contexts/ConnectionContext.tsx
@@ -108,7 +108,7 @@ export const ENDPOINTS = [
   },
   {
     name: 'devnet' as ENV,
-    endpoint: 'https://api.devnet.solana.com/',
+    endpoint: 'https://broken-morning-flower.solana-devnet.quiknode.pro/f6d9aca174b054ed7b492f89a59f0b6d0c9a7371/',
     ChainId: ChainId.Devnet,
   },
 ];
