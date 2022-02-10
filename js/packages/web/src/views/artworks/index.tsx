1a9fa208ceb2492bdd64b3d67a65e4e38fbdaefb
Dennis Lim <dennis@momenti.tv>
2022-02-10 17:10:24 +0900

    Remove top margin in artworks

--- js/packages/web/src/views/artworks/index.tsx
+++ js/packages/web/src/views/artworks/index.tsx
@@ -74,7 +74,7 @@ export const ArtworksView = () => {
   return (
     <Layout style={{ margin: 0 }} id="items-tab">
       <Content style={{ display: 'flex', flexWrap: 'wrap' }} id="items-tabs">
-        <Col style={{ width: '100%', marginTop: 10 }}>
+        <Col style={{ width: '100%' }}>
           <Row>
             <Tabs
               activeKey={activeKey}
